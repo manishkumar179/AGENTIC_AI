34e2579b208286761e5153c8a7c4b5d9cdaca5fb
manishkumar179 <mk0437271@gmail.com>
2026-08-05 21:04:13 +0530

    Create user model/schema

--- chatgpt/backend/src/config/db.js
+++ chatgpt/backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose  from "mongoose";
 
 export const connectDB = async ()=>{
     try {
-        await mongoose.connect("mongodb://0.0.0.0/genai_chatgpt")
+        await mongoose.connect("mongodb://0.0.0.0/chatgpt")
         console.log("Database connected successfully")
     } catch (error) {
         console.log("Database disconnected")
--- chatgpt/backend/src/model/user.model.js
+++ chatgpt/backend/src/model/user.model.js
@@ -1,10 +1,28 @@
 import mongoose, { mongo } from "mongoose";
-import { required } from "zod/mini";
+
 
 const userSchema = new mongoose.Schema(
     {
     name:{
         type:String,
         required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:6
     }
+},{
+    timestamps:true
 })
+
+let UserModel = mongoose.model("user", userSchema)
+
+ export default UserModel;
